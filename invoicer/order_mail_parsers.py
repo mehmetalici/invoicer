fa06b24d4ef703c327dd6ee74c914a8d394be541
Mehmet Alici <calientesea@gmail.com>
2023-06-27 12:07:26 +0200

    fix parsed mail error

--- invoicer/order_mail_parsers.py
+++ invoicer/order_mail_parsers.py
@@ -127,6 +127,7 @@ def order_from_mail(mail: Mail):
     """
     Find parser of provider and parse.
     """
+    mail = mail.mail
     parser = OrderMailParser(subject=mail.subject, body=mail.plain_text)
     order = Order(
         source_mail=mail,
